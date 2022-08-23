c68bbf663b9457cae0bc0b9d1942be90596c8ca6
Татьяна <yuki-shuichi@mail.ru>
2022-08-23 15:58:04 +0300

    Задача №13

---
+++ Task#10/Task#13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+string str = Console.ReadLine();
+int n = str.Length;
+
+if (n > 2)
+{
+  Console.WriteLine(str [2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет :( "); 
+}
