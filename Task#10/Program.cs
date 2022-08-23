6a05c2425d08409e2f06204a9bc31af9f4d473b3
Татьяна <yuki-shuichi@mail.ru>
2022-08-23 16:22:20 +0300

    Задача №15

--- Task#10/Program.cs
+++ Task#10/Program.cs
@@ -1,4 +1,11 @@
 ﻿Console.WriteLine("Введите трёхзначное число: ");
 int a = int.Parse(Console.ReadLine());
+
+while (a < 99 || a > 999)
+ {
+    Console.WriteLine("Введите трёхзначное число: ");
+    a = int.Parse(Console.ReadLine());
+ }
+
 int b = (a % 100);
 Console.WriteLine(b / 10);
---
+++ Task#10/Task#15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите цифру от 1 до 7: ");
+int num = int.Parse(Console.ReadLine());
+while (num > 7 || num < 1)
+ {
+    Console.WriteLine("Введите цифру от 1 до 7: ");
+    num = int.Parse(Console.ReadLine());
+ }
+
+if (num == 6 || num == 7)
+{
+    Console.WriteLine("Выходной :)");
+}
+else
+{
+    Console.WriteLine("Будний :(");
+}
